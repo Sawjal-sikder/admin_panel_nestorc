7475a473f26e916f119c72fe7c4481d4cff91b3b
Sawjal <mdsajalsikder@gmail.com>
2025-08-17 15:05:55 +0600

    feat: Add logo image to Navbar component and update header title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,40 +59,19 @@ const Navbar = ({ showDrawer }) => {
               // onClick={hinddleOnClick}
               className="flex items-center space-x-1 lg:space-x-2 xl:space-x-3 logo-container cursor-pointer lg:mr-4"
             >
-              <div className="relative hidden lg:block">
-                <div className="w-12 h-12 rounded-xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center shadow-lg transform rotate-3 hover:rotate-0 transition-transform duration-300">
-                  <svg
-                    className="w-7 h-7 text-white transform -rotate-3"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z" />
-                  </svg>
-                </div>
-                <div
-                  className="absolute -top-1 -right-1 w-4 h-4 bg-[#00ff8c] rounded-full animate-pulse"
-                  style={{ animation: "pulse-glow 2s ease-in-out infinite" }}
-                ></div>
-              </div>
+              {/* Header Logo */}
               <div className="mt-3">
-                <h2 className="text-2xl font-bold logo-gradient leading-tight">
-                  ViralScript
-                  <span className="text-[#ff3898]">Library</span>
+                <h2 className="text-3xl font-bold logo-gradient leading-tight">
+                  Nestorc
                 </h2>
               </div>
             </button>
 
-            {/* <Link 
-              to="/" 
-              className="text-3xl font-bold  text-purple-500 whitespace-nowrap"
-            >
-                ViralScriptLibrary
-            </Link> */}
           </div>
 
           {/* Right section */}
           <div className="flex items-center gap-4 lg:gap-8">
-            <Badge
+            {/* <Badge
               count={10}
               size="small"
               className="cursor-pointer p-2 rounded-full bg-gray-300 hover:text-blue-500 transition-colors"
@@ -101,7 +80,7 @@ const Navbar = ({ showDrawer }) => {
                 className="text-2xl"
                 onClick={() => setDrawerVisible(true)}
               />
-            </Badge>
+            </Badge> */}
 
             <Dropdown
               menu={{ items: profileMenuItems }}
