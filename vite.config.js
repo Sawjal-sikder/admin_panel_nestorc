50cb7fe2027d75e04fb5bcd6a56c4d22d07b8933
Sawjal <mdsajalsikder@gmail.com>
2025-09-06 13:34:08 +0600

    refactor: Update build configuration in vite.config.js to specify output directory

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/", // Replace with your repository name
+  build: {
+    outDir: "../build",
+  },
 });
